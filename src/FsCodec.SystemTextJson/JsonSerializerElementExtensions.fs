a5dc0c9980ee11d49b0212eb9a0a62482fd1d3c4
Nick Darvey <nickdarvey@outlook.com.au>
2020-03-31 00:43:26 +1100

    Makes things compile * Replaces Newtonsoft bits from scaffold with the actual STJ bits * Tries to retain symmetry with Newtonsoft UnionConverter impl

--- src/FsCodec.SystemTextJson/JsonSerializerElementExtensions.fs
+++ src/FsCodec.SystemTextJson/JsonSerializerElementExtensions.fs
@@ -7,19 +7,30 @@ open System.Text.Json
 
 [<AutoOpen>]
 module internal JsonSerializerExtensions =
+#if NETSTANDARD2_1
+    let private write (element : JsonElement )=
+        let bufferWriter = ArrayBufferWriter<byte>()
+        (
+            use jsonWriter = new Utf8JsonWriter(bufferWriter)
+            element.WriteTo(jsonWriter)
+        )
+        bufferWriter.WrittenSpan
+#endif
+
     type JsonSerializer with
         static member SerializeToElement(value: 'T, [<Optional; DefaultParameterValue(null)>] ?options: JsonSerializerOptions) =
             JsonSerializer.Deserialize<JsonElement>(ReadOnlySpan.op_Implicit(JsonSerializer.SerializeToUtf8Bytes(value, defaultArg options null)))
 
         static member DeserializeElement<'T>(element: JsonElement, [<Optional; DefaultParameterValue(null)>] ?options: JsonSerializerOptions) =
 #if NETSTANDARD2_1
-            let bufferWriter = ArrayBufferWriter<byte>()
-            (
-                use jsonWriter = new Utf8JsonWriter(bufferWriter)
-                element.WriteTo(jsonWriter)
-            )
-            JsonSerializer.Deserialize<'T>(bufferWriter.WrittenSpan, defaultArg options null)
+            JsonSerializer.Deserialize<'T>(write element, defaultArg options null)
 #else
-            let json = element.GetRawText()
-            JsonSerializer.Deserialize<'T>(json, defaultArg options null)
+            JsonSerializer.Deserialize<'T>(element.GetRawText(), defaultArg options null)
+#endif
+
+        static member DeserializeElement(element : JsonElement, t : Type, [<Optional; DefaultParameterValue(null)>] ?options: JsonSerializerOptions) =
+#if NETSTANDARD2_1
+            JsonSerializer.Deserialize(write element, t, defaultArg options null)
+#else
+            JsonSerializer.Deserialize(element.GetRawText(), t, defaultArg options null)
 #endif
